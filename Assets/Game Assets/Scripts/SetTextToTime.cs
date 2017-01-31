979c76b7e4159c3dce5f007eae0bf3516ff4cedc
Tom Dobbelaere <tom.dobbelaere@outlook.com>
2017-02-01 00:39:31 +0100

    Some intro work

---
+++ Assets/Game Assets/Scripts/SetTextToTime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using AC.TimeOfDaySystemFree;
+
+public class SetTextToTime : MonoBehaviour {
+	private Text myText;
+	private TimeOfDayManager timeScript;
+
+	// Use this for initialization
+	void Start () {
+		myText = GetComponent<Text> ();
+		timeScript = GameObject.Find ("TimeManager").GetComponent<TimeOfDayManager> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int hours = (int)Mathf.Floor (timeScript.timeline);
+		int minutes = (int) ((timeScript.timeline - (int)Mathf.Floor (timeScript.timeline)) * 60.0f);
+
+		myText.text = hours.ToString("00") + ":" + minutes.ToString("00");
+	}
+}
---
+++ Assets/Game Assets/Scripts/TimeController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeController : MonoBehaviour {
+	private Light lightComponent;
+	private Vector3 lightAngles;
+
+	// Use this for initialization
+	void Start () {
+		lightComponent = GetComponent<Light> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lightAngles.x = lightAngles.x + 0.5f;
+		lightAngles.y = 180;
+		lightAngles.z = 180;
+
+		transform.rotation = Quaternion.Euler (lightAngles);
+	}
+}
